8f52202bfa76f61526923919dc8825b64d35970b
Lofi <lofi@madebylofi.com>
2024-08-06 02:33:10 +0100

    Deploy site to GitHub Pages

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,7 @@
 // docs/.vitepress/config.js
 module.exports = {
     title: 'My VitePress Site',
-    description: 'Just playing around.'
-  }
+    description: 'Just playing around.',
+    base: '/website/'  // Replace with your repository name
+  };
   
